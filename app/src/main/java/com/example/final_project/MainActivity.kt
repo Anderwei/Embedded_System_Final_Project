e69d2e8bc575e1aa4fe33e3bfc4965fe73ddfdac
Ander <weiwei061689@gmail.com>
2021-12-19 23:13:46 +0800

    Clean Code

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -11,37 +11,18 @@ import com.example.final_project.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bottomBehavior: BottomSheetBehavior<View>
-    lateinit var bottom_sheet:FrameLayout
+
+    var isBottomSheetLoad = false
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        // Dynamic load bottom sheet
-        var inf:LayoutInflater = LayoutInflater.from(this)
-        findViewById<ViewGroup>(R.id.MainScreen).addView(inf.inflate(R.layout.bottom_sheet,findViewById<ViewGroup>(R.id.MainScreen),false))
-
-        bottom_sheet = findViewById(R.id.bottom_sheet)
-
-        bottomBehavior = BottomSheetBehavior.from(bottom_sheet)
-    }
-
-    fun  hideBottomSheet(){
-        bottomBehavior.isHideable=true
-        bottomBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-
-    }
-    fun showBottomSheet(v: View) {
-        bottomBehavior.isHideable=false
-        setBottomViewVisible(bottomBehavior.state != BottomSheetBehavior.STATE_EXPANDED)
-    }
-
-    private fun setBottomViewVisible(showFlag: Boolean) {
-
-        if (showFlag)
-            bottomBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-        else
-            bottomBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        if(isBottomSheetLoad){
+            // Dynamic load bottom sheet
+            var inf:LayoutInflater = LayoutInflater.from(this)
+            findViewById<ViewGroup>(R.id.MainScreen).addView(inf.inflate(R.layout.bottom_sheet,findViewById<ViewGroup>(R.id.MainScreen),false))
+        }
     }
 }
