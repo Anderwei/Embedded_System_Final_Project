655696292e59a897cbde0d5e7da61fc8c8b5e5b9
Paul0105 <paul1700152@gmail.com>
2021-12-20 23:47:02 +0800

    update MainActivity

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -5,24 +5,72 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.final_project.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import org.w3c.dom.Text
+
 
 class MainActivity : AppCompatActivity() {
 
     var isBottomSheetLoad = true
 
+    val test = arrayOf(1,2,3,4,5,6,7,8,9,10)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        class MyViewHolder(itemView: View/*, private val listener: MyAdapter.OnItemClickListener?*/) : RecyclerView.ViewHolder(itemView){
+            public var textView: TextView = itemView.findViewById(R.id.textView)
+
+            fun setData(data: Int){
+                textView.text = data.toString()
+//                itemView.setOnClickListener {
+//                    listner?.onItemClick(data)
+//                }
+            }
+        }
+
+        class MyAdapter :  RecyclerView.Adapter<MyViewHolder>(){
+//            var clickListener: AdapterView.OnItemClickListener? = null
+//
+//            fun setListener(listener: AdapterView.OnItemClickListener?) {
+//                this.clickListener = listener
+//            }
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+                return MyViewHolder(
+                        LayoutInflater.from(parent.context).inflate(
+                                R.layout.bottom_sheet,
+                                parent,
+                                false
+                        ) //, listener = clickListener
+                )
+            }
+            override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+                holder.setData(test[position])
+            }
+            override fun getItemCount(): Int {
+                return test.size
+            }
+        }
+
 
         if(isBottomSheetLoad){
+
             // Dynamic load bottom sheet
             var inf:LayoutInflater = LayoutInflater.from(this)
             findViewById<ViewGroup>(R.id.MainScreen).addView(inf.inflate(R.layout.bottom_sheet,findViewById<ViewGroup>(R.id.MainScreen),false))
+
+            val recyclerView:RecyclerView = findViewById<RecyclerView>(R.id.my_list)
+            recyclerView.adapter = MyAdapter()
+            recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+//
+//            val myAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,test)
+//            var my_list = findViewById<ListView>(R.id.my_list)
+//            my_list.adapter = myAdapter
         }
     }
 }
