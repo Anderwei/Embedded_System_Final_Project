2683b1c1ada86205b40fd46c51ef06f40b084a0f
Paul0105 <paul1700152@gmail.com>
2021-12-22 16:19:57 +0800

    Modify MainActivity

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -1,67 +1,31 @@
 package com.example.final_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.final_project.R
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import org.w3c.dom.Text
 import java.util.*
 
-
 class MainActivity : AppCompatActivity() {
-
-    var isBottomSheetLoad = true
-    lateinit var OptionImg : Array<Int>
-    lateinit var OptionText : Array<String>
-    lateinit var testList : ArrayList<Item>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        OptionImg = arrayOf(
-                R.drawable.pathfinding,
-                R.drawable.optimize,
-                R.drawable.datamining
-        )
-
-        OptionText = arrayOf(
-                "PathFinding",
-                "Optimization",
-                "DataMining"
-        )
-
-        testList = arrayListOf<Item>()
-
-        for(i in 0..2){
-            val temp = Item(OptionImg[i],OptionText[i])
-            testList.add(temp)
-        }
-
-
-        if (isBottomSheetLoad) {
-
-            // Dynamic load bottom sheet
-            var inf: LayoutInflater = LayoutInflater.from(this)
-            findViewById<ViewGroup>(R.id.MainScreen).addView(inf.inflate(R.layout.bottom_sheet, findViewById<ViewGroup>(R.id.MainScreen), false))
-
-            val recyclerView: RecyclerView = findViewById<RecyclerView>(R.id.my_list)
-            recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-            val adapter = MyAdapter(testList)
-            recyclerView.adapter = adapter
-            adapter.setOnItemClickListener(object : MyAdapter.onItemClickListener{
-                override fun onItemClick(position: Int){
-//                    Toast.makeText(this@MainActivity,testList[position],Toast.LENGTH_SHORT).show()
-                }
-            })
-
-        }
     }
+
+    fun startIntro(view: View){
+        val introActivity : Intent = Intent(this,intro::class.java)
+        startActivity(introActivity)
+    }
+
+    //    override fun onStart() {
+    //        super.onStart()
+    //        overridePendingTransition(R.anim.main_out,R.anim.intro_start)
+    //    }
 }
