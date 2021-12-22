bbdff4d55702d194c1e91f7ac697f4e6e07a20ac
Paul0105 <paul1700152@gmail.com>
2021-12-22 22:50:50 +0800

    Update text content

--- app/src/main/java/com/example/final_project/MyAdapter.kt
+++ app/src/main/java/com/example/final_project/MyAdapter.kt
@@ -33,7 +33,7 @@ class MyAdapter(private val data: ArrayList<Item>) :  RecyclerView.Adapter<MyVie
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentItems = data[position % 4]
+        val currentItems = data[position % data.size]
         holder.textView.text = currentItems.t
 //        holder.textView.background = currentItems.img
         holder.imageView.setImageResource(currentItems.img)
