a6533291757b056f7b525af9d6305762c34f67c6
Paul0105 <paul1700152@gmail.com>
2021-12-22 17:15:49 +0800

    Add Activity Transition

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.final_project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Scene
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
@@ -16,16 +18,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 
     fun startIntro(view: View){
         val introActivity : Intent = Intent(this,intro::class.java)
-        startActivity(introActivity)
+        startActivity(introActivity,ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle())
     }
 
-    //    override fun onStart() {
-    //        super.onStart()
-    //        overridePendingTransition(R.anim.main_out,R.anim.intro_start)
-    //    }
 }
--- app/src/main/java/com/example/final_project/intro.kt
+++ app/src/main/java/com/example/final_project/intro.kt
@@ -2,6 +2,7 @@ package com.example.final_project
 
 import android.content.Intent
 import android.os.Bundle
+import android.transition.Fade
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
@@ -11,15 +12,18 @@ import java.util.ArrayList
 
 data  class Item(var img: Int,var t : String) {}
 
-class intro : AppCompatActivity(){
+class intro : AppCompatActivity() {
     var isBottomSheetLoad = true
-    lateinit var OptionImg : Array<Int>
-    lateinit var OptionText : Array<String>
-    lateinit var testList : ArrayList<Item>
+    lateinit var OptionImg: Array<Int>
+    lateinit var OptionText: Array<String>
+    lateinit var testList: ArrayList<Item>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.intro_layout)
+
+        init()
+
         OptionImg = arrayOf(
                 R.drawable.pathfinding,
                 R.drawable.optimization,
@@ -34,10 +38,11 @@ class intro : AppCompatActivity(){
 
         testList = arrayListOf<Item>()
 
-        for(i in 0..2){
-            val temp = Item(OptionImg[i],OptionText[i])
+        for (i in 0..2) {
+            val temp = Item(OptionImg[i], OptionText[i])
             testList.add(temp)
         }
+
 
         if (isBottomSheetLoad) {
 
@@ -49,19 +54,21 @@ class intro : AppCompatActivity(){
             recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
             val adapter = MyAdapter(testList)
             recyclerView.adapter = adapter
-            adapter.setOnItemClickListener(object : MyAdapter.onItemClickListener{
-                override fun onItemClick(position: Int){
-                    val intent = Intent(this@intro,showActivity::class.java)
-                    intent.putExtra("title",testList[position % 3].t)
-                    intent.putExtra("img" ,testList[position % 3].img)
+            adapter.setOnItemClickListener(object : MyAdapter.onItemClickListener {
+                override fun onItemClick(position: Int) {
+                    val intent = Intent(this@intro, showActivity::class.java)
+                    intent.putExtra("title", testList[position % 3].t)
+                    intent.putExtra("img", testList[position % 3].img)
                     startActivity(intent)
                 }
             })
         }
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//        overridePendingTransition(R.anim.main_out,R.anim.intro_start)
-//    }
+    private fun init() {
+        val fadeTransition = Fade()
+        fadeTransition.duration = 1500
+        window.enterTransition = fadeTransition
+        window.exitTransition = fadeTransition
+    }
 }
